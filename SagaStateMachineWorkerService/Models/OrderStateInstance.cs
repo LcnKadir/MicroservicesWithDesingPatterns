37276e8bb7a3c51a172c585af069cbed12516a68
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-02 11:07:54 +0300

    SagaStateMachineWorkerService [Models/OrderStateInstance]

---
+++ SagaStateMachineWorkerService/Models/OrderStateInstance.cs
@@ -0,0 +1,42 @@
+﻿using Automatonymous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; }
+        public string BuyerId { get; set; }
+        public int OrderId { get; set; }
+
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public override string ToString()
+        {
+            var properties = GetType().GetProperties();
+
+            var sb = new StringBuilder();
+
+            properties.ToList().ForEach(p =>
+            {
+                var value = p.GetValue(this, null);
+                sb.Append($"{p.Name}:{value}");
+            });
+
+            sb.Append("--------------------");
+
+            return sb.ToString();
+        }
+    }
+}
