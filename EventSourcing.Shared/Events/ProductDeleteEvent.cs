e12fcdd131f6ad96fc26fa27806c16be004d5788
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-05 16:04:47 +0300

    Events [ProductDeleteEvent]

---
+++ EventSourcing.Shared/Events/ProductDeleteEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Shared.Events
+{
+    public class ProductDeleteEvent: IEvent
+    {
+        public Guid Id { get; set; }
+    }
+}
