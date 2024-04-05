0f87ca754819d4c724920666c7e27415269aa59d
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-05 16:14:13 +0300

    DTOs [CreatProductDto]

---
+++ EventSourcing.API/DTOs/CreatProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace EventSourcing.API.DTOs
+{
+    public class CreatProductDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int Stock  { get; set; }
+        public decimal Price { get; set; }
+    }
+}
