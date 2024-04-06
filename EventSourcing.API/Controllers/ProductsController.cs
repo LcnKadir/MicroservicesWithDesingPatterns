a047f4555a03c57d72d51de02d0e188320e7f847
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-06 16:50:59 +0300

    ProductsController.cs created

---
+++ EventSourcing.API/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using EventSourcing.API.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcing.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+       
+    }
+}
