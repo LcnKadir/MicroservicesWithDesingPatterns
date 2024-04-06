29583044af7672f2f619e8d73d72648303be01ae
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-06 16:46:01 +0300

    Handlers [ChangeProductPriceCommandHandler]

---
+++ EventSourcing.API/Handlers/ChangeProductPriceCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using EventSourcing.API.Commands;
+using EventSourcing.API.EventStores;
+using MediatR;
+
+namespace EventSourcing.API.Handlers
+{
+    public class ChangeProductPriceCommandHandler : IRequestHandler<ChangePorductPriceCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public ChangeProductPriceCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(ChangePorductPriceCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.PriceChanged(request.ChangeProductPriceDto);
+            await _productStream.SaveAsync();
+            return Unit.Value;
+        }
+    }
+}
