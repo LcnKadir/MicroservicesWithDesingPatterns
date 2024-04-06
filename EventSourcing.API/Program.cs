469dd65ca5b2e9880337dfb80b92f80aa907dddf
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-06 18:04:53 +0300

    Models [AppDbContext] was created and SQL database connection was established by migration with program.cs.

--- EventSourcing.API/Program.cs
+++ EventSourcing.API/Program.cs
@@ -1,10 +1,23 @@
 using EventSourcing.API.EventStores;
+using EventSourcing.API.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
 
 
 builder.Services.AddEventStore(builder.Configuration);
@@ -13,8 +26,7 @@ builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddSingleton<ProductStream>(); //Events will be reflected to the database with ProductStream. //ProductStream ile beraber Event'lerin veri tabanýna yansýmasý saðlanacak.
 
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
