5d9e8504100cd83b362a4b831b305221d6fade39
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-06 15:22:23 +0300

    EvenStrores [AbstrackStream]

---
+++ EventSourcing.API/EventStores/AbstrackStream.cs
@@ -0,0 +1,11 @@
+﻿using EventSourcing.Shared.Events;
+
+namespace EventSourcing.API.EventStores
+{
+    public abstract class AbstrackStream
+    {
+        protected readonly LinkedList<IEvent> Events = new LinkedList<IEvent>();
+
+        protected string StreamName { get;}
+    }
+}
