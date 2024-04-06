7becc0f177a71ff4b640ff819a064ee93fc55eaa
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-06 15:53:57 +0300

    EventStoreExtensions and Progmra.cs

---
+++ EventSourcing.API/EventStores/EventStoreExtensions.cs
@@ -0,0 +1,35 @@
+﻿using EventStore.ClientAPI;
+using System.Runtime.CompilerServices;
+
+namespace EventSourcing.API.EventStores
+{
+    public static class EventStoreExtensions
+    {
+        public static void AddEventStore(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = EventStoreConnection.Create(connectionString: configuration.GetConnectionString("EventStore"));
+
+            connection.ConnectAsync().Wait();
+
+            services.AddSingleton(connection);
+
+            using var logFactory = LoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Information);
+                builder.AddConsole();
+            });
+
+            var logger = logFactory.CreateLogger("Program");
+
+            connection.Connected += (sender, args) =>
+            {
+                logger.LogInformation("EventStore connection established");
+            };
+
+            connection.ErrorOccurred += (sender, args) =>
+            {
+                logger.LogError(args.Exception.Message);
+            };
+        }
+    }
+}
--- EventSourcing.API/Program.cs
+++ EventSourcing.API/Program.cs
@@ -1,6 +1,13 @@
+using EventSourcing.API.EventStores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddEventStore(builder.Configuration);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
