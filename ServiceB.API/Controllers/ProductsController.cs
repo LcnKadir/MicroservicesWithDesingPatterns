818006ad7b273181e2e2c32013ff932e030b0626
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-11 14:04:14 +0300

    ServiceB.API [ProductsController]

---
+++ ServiceB.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetProduct (int id)
+        {
+            return Ok(new { Id = id, Name = "Kalem", Price = 100, Stock = 200, Category = "Kalemler" });
+        }
+    }
+}
