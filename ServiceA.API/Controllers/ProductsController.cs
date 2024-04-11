182f709be8e44a1e20cd3aac3af28779ebb1fdc9
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-11 14:21:43 +0300

    ServiceA.API [ProductsController]

---
+++ ServiceA.API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _productService.GetProduct(id));
+        }
+    }
+}
