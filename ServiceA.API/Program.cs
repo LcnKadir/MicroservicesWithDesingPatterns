7a8790b8b0122a6c503d70aafeeb642bbd84c140
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-11 17:50:18 +0300

    ServiceA.Apı - Programcs [Retry]

--- ServiceA.API/Program.cs
+++ ServiceA.API/Program.cs
@@ -1,4 +1,10 @@
+
+
+using Polly;
+using Polly.Extensions.Http;
 using ServiceA.API;
+using System.Diagnostics;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,15 +19,33 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ProductService>(opt =>
 {
     opt.BaseAddress = new Uri("https://localhost:5003/api/products/");
-});
+}).AddPolicyHandler(GetRetryPolicy());
 
 var app = builder.Build();
+
+
+//Retrypolicy
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
+    {
+        Debug.WriteLine($"Retry Count {retryAttempt}:");
+        return TimeSpan.FromSeconds(10);
+    }, onRetryAsync: onretryAsync);
+}
+Task onretryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+{
+    Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds} ");
+    return Task.CompletedTask;
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
