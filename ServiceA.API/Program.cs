c299f753843b223ea2f99fa87bec127ed3883cdd
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-11 14:15:07 +0300

    RetryPattern - ServiceA.API / Models[Product] and ProductService

--- ServiceA.API/Program.cs
+++ ServiceA.API/Program.cs
@@ -1,9 +1,18 @@
+using ServiceA.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddHttpClient<ProductService>(opt =>
+{
+    opt.BaseAddress = new Uri("https://localhost:5003/api/products");
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
